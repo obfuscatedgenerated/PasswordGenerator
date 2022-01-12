2855e6cdadd7e167708ed6c88600562446c6b5fd
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-12 23:50:49 +0000

    switch to bxml - test

--- src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
+++ src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
@@ -1,31 +1,21 @@
 package ml.obfuscatedgenerated.PasswordGenerator;
 
-import java.awt.Color;
-import java.awt.Font;
-
+import com.sun.tools.javac.Main;
+import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.collections.Map;
-import org.apache.pivot.wtk.*;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
 
 public class MainWindow implements Application {
     private Window window = null;
 
     @Override
-    public void startup(Display display, Map<String, String> properties) {
-        window = new Window();
-
-        Label label = new Label();
-        label.setText("Hello World!");
-        label.getStyles().put("font", new Font("Arial", Font.BOLD, 24));
-        label.getStyles().put("color", Color.RED);
-        label.getStyles().put("horizontalAlignment",
-                HorizontalAlignment.CENTER);
-        label.getStyles().put("verticalAlignment",
-                VerticalAlignment.CENTER);
-
-        window.setContent(label);
-        window.setTitle("Hello World!");
-        window.setMaximized(true);
-
+    public void startup(Display display, Map<String, String> properties)
+            throws Exception {
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window)bxmlSerializer.readObject(MainWindow.class, "PassGen.bxml");
         window.open(display);
     }
 
