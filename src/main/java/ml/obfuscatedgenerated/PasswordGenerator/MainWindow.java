e4f38df2a5f1198ccbd981b60981cd688ffbe822
Toby Bridle <bridle.toby@gmail.com>
2022-01-14 22:50:09 +0000

    Fixed Menu Bar Title for MacOS

--- src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
+++ src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
@@ -55,6 +55,7 @@ public class MainWindow implements Application {
     }
 
     public static void main(String[] args) {
+	System.setProperty("apple.awt.application.name", "Password Generator");
         DesktopApplicationContext.main(MainWindow.class, args);
     }
 }
