b9a99030b1aa2dc167a11b294b05fd704e50224b
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-17 23:34:30 +0000

    length input and validation

--- src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
+++ src/main/java/ml/obfuscatedgenerated/PasswordGenerator/MainWindow.java
@@ -3,7 +3,11 @@ package ml.obfuscatedgenerated.PasswordGenerator;
 import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.*;
+import org.apache.pivot.wtk.validation.IntValidator;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +15,7 @@ public class MainWindow implements Application {
     private Window window = null;
     private TextInput passGenOutput = null;
     private final Generator gen = new Generator();
+    private TextInput lengthInput = null;
 
     private final Runnable setSize = (() -> DesktopApplicationContext.sizeHostToFit(window));
 
@@ -26,13 +31,39 @@ public class MainWindow implements Application {
         genButton.getButtonPressListeners().add(mkPassListener);
         PushButton copyButton = (PushButton) ns.get("copyButton");
         copyButton.getButtonPressListeners().add(copyListener);
+        lengthInput = (TextInput) ns.get("lengthInput");
+        lengthInput.setValidator(new IntValidator());
         DesktopApplicationContext.scheduleRecurringCallback(setSize, 1);
     }
 
+    private boolean validate () {
+        boolean[] boolchecks = new boolean[]{
+                lengthInput.isTextValid(),
+                true // add more validation booleans in place of this
+        };
+        return !Arrays.toString(boolchecks).contains("false");
+    }
+
+    private void playsound(String path) {
+        File sound_f = new File(path);
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(sound_f));
+            clip.start();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     private final ButtonPressListener mkPassListener = button -> {
-        // TODO: user options (length, structure)
+        // TODO: user options (length [DONE], structure)
         // TODO: xkcd mode (with custom delimiters)
         // TODO: chunked mode with custom delimiters (e.g. 12345_abcde_@}{:?)
+        if (!validate()) {
+            System.out.println("Validation error!");
+            playsound("./src/main/resources/ml/obfuscatedgenerated/PasswordGenerator/error.wav");
+            return;
+        }
         List<String> includes = Arrays.asList("alpha", "upper", "numeral", "symbol");
         passGenOutput.setText(gen.generate(includes, 16));
     };
