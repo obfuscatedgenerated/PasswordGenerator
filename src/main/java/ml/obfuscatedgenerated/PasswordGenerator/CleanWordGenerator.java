f8616c9265acffecf3870351c2dfd168413ce2ff
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-08-13 00:43:47 +0100

    slow but steady xkcd mode

--- src/main/java/ml/obfuscatedgenerated/PasswordGenerator/CleanWordGenerator.java
+++ src/main/java/ml/obfuscatedgenerated/PasswordGenerator/CleanWordGenerator.java
@@ -1,13 +1,26 @@
 package ml.obfuscatedgenerated.PasswordGenerator;
 
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class CleanWordGenerator {
-    private static ArrayList<String> is2al(InputStream is) throws IOException {
-        BufferedReader br = null;
+    private final SnowballStemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
+    private final ArrayList<String> dictionary;
+    private final ArrayList<String> badwords;
+
+    public CleanWordGenerator() throws IOException {
+        InputStream dict_is = CleanWordGenerator.class.getResourceAsStream("dictionary.txt");
+        dictionary = is2al(dict_is);
+        InputStream bw_is = CleanWordGenerator.class.getResourceAsStream("dictionary.txt");
+        badwords = is2al(bw_is);
+    }
+
+    private ArrayList<String> is2al(InputStream is) throws IOException {
+        BufferedReader br;
         ArrayList<String> arr = new ArrayList<>();
         br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
         String line;
@@ -15,19 +28,15 @@ public class CleanWordGenerator {
             arr.add(line);
         return arr;
     }
-    public static String generate_unfiltered() throws IOException { // this is probably very inefficient (actually, not terrible, but can still be improved at a later date)
-        InputStream is = CleanWordGenerator.class.getResourceAsStream("dictionary.txt");
-        ArrayList<String> arr = is2al(is);
-        Random rng = new Random(); // would use RandomAccessFile, but we are using resources
-        int line = rng.nextInt(arr.size());
-        return arr.get(line).toLowerCase();
+    public String generate_unfiltered() { // this is probably very inefficient (actually, not terrible, but can still be improved at a later date)
+        SecureRandom rng = new SecureRandom(); // would use RandomAccessFile, but we are using resources
+        int line = rng.nextInt(dictionary.size());
+        return (String) stemmer.stem(dictionary.get(line).toLowerCase());
     }
-    public static String generate_filtered() throws IOException { // could just provide a filtered wordlist, but this is fine for now
-        InputStream is = CleanWordGenerator.class.getResourceAsStream("badwords.txt");
-        ArrayList<String> arr = is2al(is);
+    public String generate_filtered() { // could just provide a filtered wordlist, but this is fine for now
         genLoop: while (true) {
             String s = generate_unfiltered();
-            for (String i : arr) {
+            for (String i : badwords) {
                 if (s.matches(i)) {
                     continue genLoop;
                 }
